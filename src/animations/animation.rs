3aa024d7ddb62edb78bfa7bb88765c7858081a91
Ben <ben.arteii@proton.me>
2024-03-22 23:46:44 +0100

    use crossterm fully

--- src/animations/animation.rs
+++ src/animations/animation.rs
@@ -1,4 +1,8 @@
-use crossterm::{cursor, execute, style::{Color, Print, ResetColor, SetForegroundColor}, terminal};
+use crossterm::{
+    cursor, execute,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal,
+};
 use std::io::{stdout, Write};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -20,32 +24,51 @@ fn go_back_print_flush(text: &str) {
         cursor::MoveToColumn(0),
         Print(text),
     )
-        .unwrap();
+    .unwrap();
 }
 
 /// A loading animation that runs in a separate thread.
 pub fn spinner_animation(frames: &Frames, should_stop: Arc<Mutex<bool>>) {
     let mut frame_index = 0;
-    let longest_frame_len = frames.frames.iter().map(|frame| frame.len()).max().unwrap_or(0);
+    let longest_frame_len = frames
+        .frames
+        .iter()
+        .map(|frame| frame.len())
+        .max()
+        .unwrap_or(0);
 
-    execute!(stdout(), cursor::Hide).unwrap(); // Hide cursor
+    execute!(stdout(), cursor::Hide).unwrap(); // hide cursor
 
     while !*should_stop.lock().unwrap() {
         let frame = &frames.frames[frame_index];
-        go_back_print_flush(frame);
+
+        // Clear the current line, move the cursor to the beginning of the line, print the frame, and flush stdout
+        execute!(
+            stdout(),
+            terminal::Clear(terminal::ClearType::CurrentLine),
+            cursor::MoveToColumn(0),
+            Print(frame),
+            Print("\r"),
+        )
+        .unwrap();
 
         thread::sleep(Duration::from_millis(frames.speed_ms));
-        print_flush("\r");
 
         frame_index = (frame_index + 1) % frames.frames.len();
     }
 
     let clear_length = " ".repeat(longest_frame_len);
-    go_back_print_flush(&clear_length);
 
-    execute!(stdout(), cursor::Show).unwrap(); // Show cursor
-    print_flush("\r"); // Set to beginning of line
+    // Clear the line after the animation finishes
+    execute!(
+        stdout(),
+        terminal::Clear(terminal::ClearType::CurrentLine),
+        cursor::MoveToColumn(0),
+        Print(&clear_length),
+        Print("\r"),
+        cursor::Show, // show cursor
+    )
+    .unwrap();
 }
-
 
 // TODO: implement loading bar animation
