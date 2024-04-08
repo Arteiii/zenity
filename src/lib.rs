84b39d91c8cd5285ef2e28f128e3c8daf70b1a4a
Ben <ben.arteii@proton.me>
2024-04-08 17:12:05 +0200

    documentation improvements

--- src/lib.rs
+++ src/lib.rs
@@ -30,24 +30,32 @@
 //! }
 //! ```
 //!
-//! check out the examples for more
+//! # Color Configuration
 //!
+//! To configure the color output, you can use the `--color` option with one of the following values:
+//! 
+//! - `always`: Enable color output regardless of the terminal type and capabilities
+//! - `auto`: Automatically determine whether to enable color output based on the terminal type and capabilities. If stdout is a pipe or if the terminal doesn't support colors, colors will be disabled
+//! - `never`: Disable color output
+//! 
+//! **Note**: If the stdout is a pipe or if the terminal doesn't support colors, colors will be automatically disabled 
+//! 
+//! Check out the examples for more.
 
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-use crate::animations::animation::Spinner;
-
-pub use crate::animations::frames::spinner;
 pub use crossterm::style;
-
-pub mod animations;
-mod helper;
 
 pub use animations::spinner as multi_spinner;
 
+use crate::animations::animation::Spinner;
+pub use crate::animations::frames::spinner;
 // define helper functions for the cross-term colors
-pub use crate::helper::colors::combine_attributes;
+pub use crate::helper::colors::{CliColorConfig, combine_attributes};
+
+pub mod animations;
+mod helper;
 
 /// `LoadingAnimation` is a struct that provides a straightforward interface for creating and managing customizable loading animations.
 ///
@@ -94,6 +102,7 @@ pub struct LoadingAnimation {
     animation_style: Arc<Mutex<style::ContentStyle>>,
     text_style: Arc<Mutex<style::ContentStyle>>,
     cleanup_on_exit: Arc<Mutex<bool>>,
+    color_support: Arc<Mutex<CliColorConfig>>,
 }
 
 impl Default for LoadingAnimation {
@@ -116,6 +125,7 @@ impl Default for LoadingAnimation {
             spinner::PreDefined::dot_spinner1(false),
             animation_style,
             text_style,
+            CliColorConfig::default(),
         )
     }
 }
@@ -137,6 +147,7 @@ impl LoadingAnimation {
             frames,
             Arc::clone(&default_animation.animation_style),
             Arc::clone(&default_animation.text_style),
+            CliColorConfig::default(),
         )
     }
 
@@ -155,12 +166,14 @@ impl LoadingAnimation {
         frames: spinner::Frames,
         animation_style_mutex: Arc<Mutex<style::ContentStyle>>,
         text_style_mutex: Arc<Mutex<style::ContentStyle>>,
+        color_support: CliColorConfig,
     ) -> Self {
         let frames = Arc::new(Mutex::new(frames));
         let cleanup_on_exit = Arc::new(Mutex::new(true));
         let should_stop = Arc::new(Mutex::new(false));
         let text = Arc::new(Mutex::new(None));
         let end_sequence = Arc::new(Mutex::new(None));
+        let color_support = Arc::new(Mutex::new(color_support));
 
         let should_stop_clone = Arc::clone(&should_stop);
         let text_clone = Arc::clone(&text);
@@ -168,6 +181,9 @@ impl LoadingAnimation {
         let animation_style_clone = Arc::clone(&animation_style_mutex);
         let text_style_clone = Arc::clone(&text_style_mutex);
         let cleanup_on_exit_clone = Arc::clone(&cleanup_on_exit);
+
+        // TODO: implement
+        // let color_support_clone = Arc::clone(&color_support);
 
         let spinner = Spinner::new(
             frames,
@@ -189,6 +205,7 @@ impl LoadingAnimation {
             text_style: text_style_mutex,
             end_sequence,
             cleanup_on_exit,
+            color_support,
         }
     }
 
@@ -203,14 +220,14 @@ impl LoadingAnimation {
     /// # Example
     ///
     /// ```
-    /// use my_spinner_lib::{spinner::PreDefined, LoadingAnimation};
-    /// let spinner = LoadingAnimation::new(PreDefined::dot_spinner1());
-    /// // Update the text content of the spinner animation.
-    /// spinner.set_text("Loading..."); // Sets the text to "Loading..."
+    /// use zenity::LoadingAnimation;
+    /// let spinner = LoadingAnimation::default();
+    /// // update the text content of the spinner animation.
+    /// spinner.set_text("Loading..."); // ets the text to "Loading..."
     /// ```
     pub fn set_text(&self, text: &str) {
         let mut guard = self.text.lock().unwrap();
-        *guard = Some(text.to_string()); // Update the text value
+        *guard = Some(text.to_string()); // update the text value
     }
 
     /// sets the color of the animation
@@ -218,15 +235,24 @@ impl LoadingAnimation {
     /// # Arguments
     ///
     /// * `color` - the color to set for the animation
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use zenity::{LoadingAnimation, style::Color};
+    /// let spinner = LoadingAnimation::default();
+    ///
+    /// spinner.set_animation_color(Color::Red); // sets the animation color to Red
+    /// ```
     pub fn set_animation_color(&self, color: style::Color) {
-        let style = style::ContentStyle {
-            foreground_color: Some(color),
-            background_color: None,
-            underline_color: None,
-            attributes: style::Attributes::default(),
-        };
-
-        *self.animation_style.lock().unwrap() = style;
+        if !self.color_support.lock().unwrap().should_enable_color() {
+            *self.animation_style.lock().unwrap() = style::ContentStyle {
+                foreground_color: Some(color),
+                background_color: None,
+                underline_color: None,
+                attributes: style::Attributes::default(),
+            };
+        }
     }
 
     /// sets the color of the text
@@ -234,25 +260,82 @@ impl LoadingAnimation {
     /// # Arguments
     ///
     /// * `color` - the color to set for the text
+    /// # Example
+    ///
+    /// ```
+    /// use zenity::{LoadingAnimation, style::Color};
+    /// let spinner = LoadingAnimation::default();
+    ///
+    /// spinner.set_text_color(Color::Red); // sets the text color to Red
+    /// ```
     pub fn set_text_color(&self, color: style::Color) {
-        let style = style::ContentStyle {
-            foreground_color: Some(color),
-            background_color: None,
-            underline_color: None,
-            attributes: style::Attributes::default(),
-        };
-
-        *self.text_style.lock().unwrap() = style;
+        if !self.color_support.lock().unwrap().should_enable_color() {
+            *self.text_style.lock().unwrap() = style::ContentStyle {
+                foreground_color: Some(color),
+                background_color: None,
+                underline_color: None,
+                attributes: style::Attributes::default(),
+            };
+        }
     }
 
+    /// sets the style of the animation
+    ///
+    /// not all styles/color can be applied to all animations
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use zenity::{LoadingAnimation, style::{Attribute, ContentStyle, Color}, combine_attributes};
+    /// let spinner = LoadingAnimation::default();
+    ///
+    /// let style = ContentStyle {
+    ///     background_color: None,
+    ///     underline_color: Some(Color::Red),
+    ///     foreground_color: Some(Color::Rgb {r: 255, g:255, b:255}),
+    ///
+    ///     // combine attributes is a helper function to combine multiple style elements and apply them
+    ///     attributes: combine_attributes(&[
+    ///         &Attribute::Bold,
+    ///         &Attribute::Underlined,
+    ///     ]),
+    /// };
+    /// spinner.set_animation_style(style); // sets the aniamtion style
+    /// ```
     pub fn set_animation_style(&self, color: style::ContentStyle) {
-        *self.animation_style.lock().unwrap() = color;
+        if !self.color_support.lock().unwrap().should_enable_color() {
+            *self.animation_style.lock().unwrap() = color;
+        }
     }
 
+    /// sets the style of the text
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use zenity::{LoadingAnimation, style::{Attribute, ContentStyle, Color}, combine_attributes};
+    /// let spinner = LoadingAnimation::default();
+    ///
+    /// let style = ContentStyle {
+    ///     background_color: None,
+    ///     underline_color: Some(Color::Red),
+    ///     foreground_color: Some(Color::Rgb {r: 255, g:255, b:255}),
+    ///
+    ///     // combine attributes is a helper function to combine multiple style elements and apply them
+    ///     attributes: combine_attributes(&[
+    ///         &Attribute::Bold,
+    ///         &Attribute::Underlined,
+    ///     ]),
+    /// };
+    /// spinner.set_text_style(style); // sets the text style
+    /// ```
     pub fn set_text_style(&self, color: style::ContentStyle) {
-        *self.text_style.lock().unwrap() = color;
+        if !self.color_support.lock().unwrap().should_enable_color() {
+            *self.text_style.lock().unwrap() = color;
+        }
     }
 
+    /// stop and delete the animation from the console
     pub fn stop_and_discard(&mut self) {
         self.finish();
     }
@@ -289,7 +372,9 @@ impl LoadingAnimation {
         }
 
         if let Some(color) = text_color {
-            *self.text_style.lock().unwrap() = color;
+            if !self.color_support.lock().unwrap().should_enable_color() {
+                *self.text_style.lock().unwrap() = color;
+            }
         }
 
         // stop the animation
