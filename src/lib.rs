805f210a2b79b7b74c1447019984683a41d7f835
Ben <ben.arteii@proton.me>
2024-04-09 21:56:03 +0200

    docs updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![warn(missing_docs)]
 #![warn(rustdoc::missing_crate_level_docs)]
-#![warn(rustdoc::missing_doc_code_examples)]
 
 //! # Yet Another Spinner Lib
 //!
@@ -16,31 +15,22 @@
 //!
 //! ```rust
 //! // example/multi_spinner.rs
-//!
 //! # use std::thread::sleep;
 //! # use std::time::Duration;
+//! use zenity::spinner::{MultiSpinner, Frames};
 //!
-//! use zenity::spinner::MultiSpinner;
-//! use zenity::spinner::PreDefined;
+//! # fn main() {
+//! let spinner = MultiSpinner::default();
+//! let spinner1 = spinner.get_last();
+//! let spinner2 = spinner.add(Frames::binary(false));
 //!
-//! fn main() {
-//!         let mut spinner = MultiSpinner::default();
-//!         let spinner1 = spinner.add(PreDefined::dot_spinner11(false));
-//!         let spinner2 = spinner.add(PreDefined::binary(false));
-//!
-//!         // access the spinner through the Arc<Mutex<MultiSpinner>> reference
-//!         spinner.run_all();
-//!
-//!         spinner.set_text(&spinner2, "spinner2".to_string());
-//!
-//!         sleep(Duration::from_secs(2));
-//!
-//!         spinner.stop(&spinner2);
-//!         spinner.set_text(&spinner2, "Successfully".to_string());
-//!
-//!         sleep(Duration::from_secs(2));
-//!         spinner.set_text(&spinner1, "spinner1 stopped".to_string());
-//!}
+//! // start all spinners
+//! spinner.run_all();
+//! # sleep(Duration::from_secs(2));
+//! 
+//! spinner.stop(&spinner2);
+//! spinner.set_text(&spinner2, "Successfully".to_string());
+//!# }
 //! ```
 //!
 //! ## Progress Bar
@@ -49,19 +39,19 @@
 //! # use rand::Rng;
 //! # use std::thread;
 //! # use std::time::Duration;
-//! use zenity::progress::{Bar, ProgressBar, ProgressBarFrames};
+//! use zenity::progress::ProgressBar;
 //!
-//! fn main() {
-//!     let progress = ProgressBar::default();
-//!     # let loading = 1_usize;
+//! # fn main() {
+//! let progress = ProgressBar::default();
+//!# let loading = 1_usize;
 //!
-//!     for loading in loading..=100 {
-//!         # thread::sleep(Duration::from_millis(rand::thread_rng().gen_range(1..=70)));
-//!         // do work...
-//!         progress.set(&progress.get_last(), &loading);
-//!     }
-//!     
+//! for loading in loading..=100 {
+//!   # thread::sleep(Duration::from_millis(rand::thread_rng().gen_range(1..=70)));
+//!   // do work...
+//!   progress.set(&progress.get_last(), &loading);
 //! }
+//!     
+//! # }
 //! ```
 //! # Color Configuration
 //!
@@ -75,7 +65,7 @@
 //!
 //! Check out the examples for more
 
-mod color;
+pub mod color;
 mod iterators;
 pub mod progress;
 pub mod spinner;
