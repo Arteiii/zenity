54ec72bb99f0e0a0d1a661ec7c44ed146a35372e
Ben <ben.arteii@proton.me>
2024-04-17 17:00:33 +0200

    reworked input example

--- examples/input.rs
+++ examples/input.rs
@@ -2,9 +2,11 @@ use regex::Regex;
 use zenity::menu::input::{valid_path, valid_regex};
 
 fn main() {
+    println!("Input Preview:");
+
     println!(
         "\n\nReturn:  {}",
-        valid_regex(Regex::new(r"^\d{3}$").unwrap())
+        valid_regex(Regex::new(r"^\d{3}$").unwrap(), Some("369"), false)
     );
-    println!("\n\nPath:  {:?}", valid_path());
+    println!("\n\nPath:  {:?}", valid_path(None, true));
 }
