e1412c021c8f49dbf13d075f58036887ea849c00
Ben <ben.arteii@proton.me>
2024-04-23 18:16:20 +0200

    update input example to work with the new input struct

--- examples/input.rs
+++ examples/input.rs
@@ -1,20 +1,27 @@
 use regex::Regex;
-use zenity::menu::Input;
+
+use zenity::menu::input::{Input, Requirements};
 
 fn main() {
     println!("Input Preview:");
 
-    println!(
-        "\n\nReturn:  {}",
-        valid_regex(
-            "Enter string:",
-            Regex::new(r"^\d{3}$").unwrap(),
-            Some("369"),
-            false
-        )
-    );
-    println!(
-        "\n\nPath:  {:?}",
-        valid_path("Enter A Valid Path:", None, true)
-    );
+    let path = Input::new("Enter Valid Path:", Requirements::default()).start();
+    let path_with_regex = Input::new(
+        "Enter a Valid .toml file",
+        Requirements::path()
+            .set_regex(Regex::new(r"\.toml\z").unwrap())
+            .set_note("", "Please Enter a Valid Path to a .toml file"),
+    )
+    .start();
+    let regex = Input::new(
+        "Enter Valid Regex (ABC):",
+        Requirements::regex(Regex::new(r"^ABC$").unwrap()),
+    )
+    .allow_force()
+    .set_default("ABC")
+    .start();
+
+    println!("Existing Path:  {:?}", path);
+    println!("Path with Regex:  {:?}", path_with_regex);
+    println!("Regex:  {:?}", regex);
 }
